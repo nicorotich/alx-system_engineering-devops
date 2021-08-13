f9cb0083d5e091c545079ae47bc5dbbc8dd080c1
nicorotich <nicholaskrotich8@gmail.com>
2021-08-13 14:34:47 +0300

    Add commas at the end of statements

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
-# creates a file in /tmp
+# creates a file in the tmp diirectory of 
+# the node 
 
-file {'/tmp/holberton':
-  path    => '/tmp/holberton';
-  mode    => '0744'
-  owner   => 'www-data'
-  group   => 'www-data'
+file { 'holberton':
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
